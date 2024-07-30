ec8ad477cea779c0ba5e79d9628a7b644e1972c2
zkj201607295 <59324800+zkj201607295@users.noreply.github.com>
2024-07-30 09:37:43 +0800

    Delete GraphClassification/run.sh

--- GraphClassification/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#JSUB -q normal
-#JSUB -n 4
-#JSUB -e error.%J
-#JSUB -o output.%J
-#JSUB -J my_job
-source /apps/software/anaconda3/bin/activate zkj
-python main.py > GraphU-Net/GraphU-Net.txt
